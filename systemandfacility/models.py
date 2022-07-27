b5a61067d1a43012813406cdb4e5db61ae1a9f0d
alex <alex@penguin>
2022-07-27 09:18:18 -0500

    Add the system model
    To create the table in our database for healthcare systems
    
    Edit the models.py in the systemandfacility module
    
    Tags:

--- systemandfacility/models.py
+++ systemandfacility/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class System(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    short_description = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True)
+
