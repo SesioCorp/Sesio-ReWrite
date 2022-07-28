3120f303b0b60725a389927e2e83aff93329ab10
philipsesio <pdp_iii@hotmail.com>
2022-07-28 09:00:13 -0500

    Added two models in systemandfacility
    Why:Added two models whichs building and floor in systemandfacility
    
    How:By using Python script
    
    Tags:Python, models

--- systemandfacility/admin.py
+++ systemandfacility/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import System, Facility
+from .models import System, Facility, Building, Floor
 
 admin.site.register(System)
 admin.site.register(Facility)
+admin.site.register(Building)
+admin.site.register(Floor)
--- systemandfacility/models.py
+++ systemandfacility/models.py
@@ -20,3 +20,21 @@ class Facility(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Building(models.Model):
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE, related_name="buildings")
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Floor(models.Model):
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE, related_name="floors")
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="floors")
+    number = models.IntegerField()
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
