7351ec2c9426b680940da3e7311fc3e77d83e27a
alex <alex@penguin>
2022-07-28 07:40:39 -0500

    Create Facility Model
    Why: To store facility information in the database
    
    How: Edit the model in the systemandfacility module and register the
    model with admin.py
    Tags: python, models, django

--- systemandfacility/admin.py
+++ systemandfacility/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import System
+from .models import System, Facility
 
 admin.site.register(System)
+admin.site.register(Facility)
--- systemandfacility/models.py
+++ systemandfacility/models.py
@@ -7,3 +7,16 @@ class System(models.Model):
     short_description = models.CharField(max_length=150)
     slug = models.SlugField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Facility(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    short_description = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True)
+    system = models.ForeignKey(System, on_delete=models.CASCADE, related_name="facilities")
+
+    def __str__(self):
+        return self.name
