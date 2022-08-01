ae68e757e4ab9e81248461bf52a78de2c2f94b6b
philipsesio <pdp_iii@hotmail.com>
2022-08-01 12:21:33 -0500

    Added Priority Model
    Why:Added a Priority Model
    
    How:Using Python script
    
    Tags:Python, Django, Model

--- workorder/models.py
+++ workorder/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from systemandfacility.models import Facility, Location
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -9,3 +9,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Priority(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+# class WorkOrder(models.Model):
+#     facility = models.ForeignKey(Facility, on_delete=models.CASCADE, related_name="work_orders")
+#     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="work_orders")
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="work_orders")
+#     brief_discription =models.CharField(max_length=50)
+#     discription = models.TextField(blank=True, null=True)
+#     priority = models.ForeignKey(Priority, on_delete=models.CASCADE, related_name="work_orders")
+#     assigned = ForeignKey
