77796befd27042dfeb686df46a9e4f2032de9094
philipsesio <pdp_iii@hotmail.com>
2022-08-03 09:54:53 -0500

    Added a model into the workorders
    Why:Added a new model into workorders which are workorder, comment, and
    time spent for devloping work order model
    
    How:Python Script
    Tags:Python, Django, Model

--- workorder/admin.py
+++ workorder/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Category, Priority
+from .models import Category, Priority, WorkOrder, Comment, TimeSpent
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Priority)
+admin.site.register(WorkOrder)
+admin.site.register(Comment)
+admin.site.register(TimeSpent)
--- workorder/models.py
+++ workorder/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from systemandfacility.models import Facility, Location
+from django.conf import settings
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -20,11 +22,28 @@ class Priority(models.Model):
 
 
 
-# class WorkOrder(models.Model):
-#     facility = models.ForeignKey(Facility, on_delete=models.CASCADE, related_name="work_orders")
-#     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="work_orders")
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="work_orders")
-#     brief_discription =models.CharField(max_length=50)
-#     discription = models.TextField(blank=True, null=True)
-#     priority = models.ForeignKey(Priority, on_delete=models.CASCADE, related_name="work_orders")
-#     assigned = ForeignKey
+class WorkOrder(models.Model):
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE, related_name="work_orders")
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="work_orders")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="work_orders")
+    brief_discription =models.CharField(max_length=50)
+    discription = models.TextField(blank=True, null=True)
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE, related_name="work_orders")
+    assigned = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="work_orders")
+    completed_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.brief_discription
+
+class Comment(models.Model):
+    comment = models.TextField()
+    workorder = models.ForeignKey(WorkOrder, on_delete=models.CASCADE, related_name="comments")
+
+
+class TimeSpent(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    workorder = models.OneToOneField(WorkOrder, on_delete=models.CASCADE, related_name="time_spent")
+
+    def __str__(self):
+        return f"{self.start_time} {self.end_time}"
