bec67f88e0a4656db8521894a6c8c7989a1dd68d
philipsesio <pdp_iii@hotmail.com>
2022-08-22 13:28:14 -0500

    Added workorder list view
    Why:Workorder listview
    
    How:Views, Templates, Django, Python
    
    Tags:

--- workorder/urls.py
+++ workorder/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, WorkOrderListView
 
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('workorder/', WorkOrderListView.as_view(), name='work_order_list'),
 ]
--- workorder/views.py
+++ workorder/views.py
@@ -1,4 +1,11 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from .models import WorkOrder
+
 
 class Home(TemplateView):
     template_name = 'index.html'
+
+class WorkOrderListView(ListView):
+    model = WorkOrder
+    template_name = "workorder_listview.html"
