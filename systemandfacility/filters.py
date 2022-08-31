ccc44e3e312cbd1c6552200b96eb27c063241dd3
philipsesio <pdp_iii@hotmail.com>
2022-08-31 10:36:10 -0500

    Added location filters.
    Why:For locatlion form.
    
    How:Views.py, filters.py, locationform.html
    
    Tags:

--- systemandfacility/filters.py
+++ systemandfacility/filters.py
@@ -1,17 +1,17 @@
-import django_filter
+import django_filters
 from .models import *
 
-class BuildingFilter(django_filter.FilterSet):
+class BuildingFilter(django_filters.FilterSet):
     class Meta:
         model_name = Building
         fields = ["facility", "name"]
 
-class FloorFilter(django_filter.FilterSet):
+class FloorFilter(django_filters.FilterSet):
     class Meta:
         model_name = Floor
         fields = ["facility", "building", "number", "name"]
 
-class DepartmentFilter(django_filter.FilterSet):
+class DepartmentFilter(django_filters.FilterSet):
     class Meta:
         model_name = Department
         fields = ["facility", "building", "floor", "name"]
--- systemandfacility/views.py
+++ systemandfacility/views.py
@@ -1,24 +1,55 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Building, Department, Floor
+from .filters import *
 
 class BuildingListView(ListView):
     model = Building
 
     def get_queryset(self):
+        if self.request.is_ajax():
+            if self.request.GET.get("facility"):
+                return self.model.objects.filter(facility_id=self.request.GET.get("facility"))
+            else:
+                return self.model.objects.none()
         queryset = self.model.objects.all()
-        return queryset
+        filter_queryset = BuildingFilter(self.request.GET, queryset=queryset)
+        return filter_queryset
+        
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return "drop_down.html"
 
 class FloorListView(ListView):
     model = Floor
 
     def get_queryset(self):
+        if self.request.is_ajax():
+            if self.request.GET.get("building"):
+                return self.model.objects.filter(building_id=self.request.GET.get("building"))
+            else:
+                return self.model.objects.none()
         queryset = self.model.objects.all()
-        return queryset
+        filter_queryset = FloorFilter(self.request.GET, queryset=queryset)
+        return filter_queryset
+        
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return "drop_down.html"
 
 class DepartmentListView(ListView):
     model = Department
 
     def get_queryset(self):
+        if self.request.is_ajax():
+            if self.request.GET.get("floor"):
+                return self.model.objects.filter(floor_id=self.request.GET.get("floor"))
+            else:
+                return self.model.objects.none()
         queryset = self.model.objects.all()
-        return queryset
+        filter_queryset = DepartmentFilter(self.request.GET, queryset=queryset)
+        return filter_queryset
+       
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return "drop_down.html"
