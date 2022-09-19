ccd763972301fd071df6674e7fdaf58ced616ab1
alex <alex@penguin>
2022-09-19 10:13:27 -0500

    Added logic to urgent work order alert on homepage
    Why: Alert should be shown when there are urgent work orders
    
    How: Added urgent work orders to the context in homepage view. Added if
    statement to the index.html
    Tags:

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from workorder.models import WorkOrder
-from users.models import CustomUser
+from workorder.models import WorkOrder, Priority
+from django.db.models import Q
 
 class Home(TemplateView):
     template_name = 'index.html'
@@ -10,7 +10,16 @@ class Home(TemplateView):
         context = super().get_context_data(**kwargs)
         queryset = WorkOrder.objects.all()
         assigned_wo = queryset.filter(status="open", assigned_to = self.request.user).count()
+        
+        urgent_assigned_wo = queryset.filter(
+            assigned_to=self.request.user,
+            status="open",
+            priority=Priority.objects.get(Q(name__startswith="U")))
+        
         context["work_order_count"] = assigned_wo
+        context["urgent_assigned_wo"] = urgent_assigned_wo
         return context
 
 
+
+        
