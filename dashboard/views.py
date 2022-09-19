1c23fcb7de611172446101c7028d3da1f4ec6c50
alex <alex@penguin>
2022-09-19 09:44:03 -0500

    Fixed bug in work order counter on homepage
    Why: Counter logic was not correct. Was including unassigned work orders
    when it should only count assigned. Was also counting unassigned
    incorrectly.
    
    How: workorders/views.py
    
    Tags:

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from workorder.models import WorkOrder
+from users.models import CustomUser
 
 class Home(TemplateView):
     template_name = 'index.html'
@@ -8,9 +9,8 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         queryset = WorkOrder.objects.all()
-        unassigned_wo = queryset.filter(status="open", assigned_to__isnull = True).count()
         assigned_wo = queryset.filter(status="open", assigned_to = self.request.user).count()
-        context["work_order_count"] = unassigned_wo + assigned_wo
+        context["work_order_count"] = assigned_wo
         return context
 
 
