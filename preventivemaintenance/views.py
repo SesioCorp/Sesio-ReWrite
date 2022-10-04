2ec3005f1c0a37fa8f7fdebd16a741dcce4bce65
alex <alex@penguin>
2022-10-04 09:08:45 -0500

    Created Preventive Maintenance List View
    Why: Creating PM Module
    
    How: views.py
    
    Tags:

--- preventivemaintenance/views.py
+++ preventivemaintenance/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import PreventiveMaintenance
+from .filters import PreventiveMaintenanceFilter
 
-# Create your views here.
+class PreventiveMaintenanceListView(ListView):
+    model = PreventiveMaintenance
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        if self.request.is_ajax():
+            filtered_queryset = PreventiveMaintenanceFilter(self.request.GET, queryset=queryset)
+            return filtered_queryset.qs
+
+        queryset = self.model.objects.all().order_by('-updated_at')
+        filtered_queryset = PreventiveMaintenanceFilter(self.request.GET, queryset=queryset)
+        return filtered_queryset.qs
+
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return 'workorder/partials/dropdown_list.html'
+        return 'preventivemaintenance_listview.html'
+
