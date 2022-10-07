6721730128ae8992cb037d4069dc4d157b2fc41e
alex <alex@penguin>
2022-10-07 10:03:07 -0500

    Added Pagination to PM list view
    Why: To display PMs properly
    
    How: preventive_maintenance_listview.html, utils_tags.py
    
    Tags:

---
+++ preventivemaintenance/templatetags/utils_tags.py
@@ -0,0 +1,28 @@
+from urllib import request
+from django import template
+from django.conf import settings
+from django.core.paginator import Paginator
+
+register = template.Library()
+
+def get_current_page_size(request, default=None):
+    page_size = default or settings.PAGINATION_DEFAULT_PAGINATION
+    
+    try:
+        page_size = int(request.GET.get("page_size"))
+    except (ValueError, TypeError):
+        pass
+
+    if page_size <= 0:
+        page_size = default or settings.PAGINATION_DEFAULT_PAGINATION
+    return min(page_size, settings.PAGINATION_MAX_SIZE)
+
+@register.simple_tag(takes_context=True)
+def qs_paginator(context, qs):
+    request = context.request
+    paginator = Paginator(qs, get_current_page_size(request))
+    page_number = request.GET.get(settings.PAGINATION_PAGE_PARAM)
+    page = paginator.get_page(page_number)
+    return page
+
+
--- preventivemaintenance/views.py
+++ preventivemaintenance/views.py
@@ -9,17 +9,17 @@ class PreventiveMaintenanceListView(ListView):
    
     def get_queryset(self):
 
-        if self.request.is_ajax():
-            queryset = self.model.objects.all()
-            filtered_queryset = PreventiveMaintenanceFilter(self.request.GET, queryset=queryset)
-            return filtered_queryset.qs
+        # if self.request.is_ajax():
+        #     queryset = self.model.objects.all()
+        #     filtered_queryset = PreventiveMaintenanceFilter(self.request.GET, queryset=queryset)
+        #     return filtered_queryset.qs
 
         queryset = self.model.objects.all()
         filtered_queryset = PreventiveMaintenanceFilter(self.request.GET, queryset=queryset)
         return filtered_queryset
 
     def get_template_names(self):
-        if self.request.is_ajax():
-            return 'workorder/partials/dropdown_list.html'
+        # if self.request.is_ajax():
+        #     return 'workorder/partials/dropdown_list.html'
         return 'preventivemaintenance_listview.html'
 
