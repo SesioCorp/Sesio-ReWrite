9849bb4c0141629991666f157879f457fb365980
alex <alex@penguin>
2022-10-15 14:00:38 -0500

    Add Update Forms to PM Detail Template
    Why: To update location and asset details
    
    How: forms.py, views.py, preventivemaintenance_detail.html
    
    Tags:

--- preventivemaintenance/forms.py
+++ preventivemaintenance/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+
+from asset.models import AssetType
 from .models import PreventiveMaintenance
 
 
@@ -6,3 +8,10 @@ class PreventiveMaintenanceForm(forms.ModelForm):
     class Meta:
         model = PreventiveMaintenance
         fields = ['slug', 'facility', 'asset', 'status', 'frequency', 'started_at', 'expired_at', 'image']
+
+class PreventiveMaintenanceAssetDetailsForm(forms.Form):
+    asset_type = forms.ModelChoiceField(label="Type", queryset=AssetType.objects.all())
+    weight = forms.IntegerField(label="Weight", required=False)
+    brand = forms.CharField(label="Brand", required=False, max_length=100)
+    
+
--- preventivemaintenance/views.py
+++ preventivemaintenance/views.py
@@ -3,6 +3,9 @@ from .models import PreventiveMaintenance
 from .filters import PreventiveMaintenanceFilter
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from systemandfacility.forms import LocationForm
+from .forms import PreventiveMaintenanceAssetDetailsForm
+from asset.models import *
 
 class PreventiveMaintenanceListView(ListView):
    
@@ -30,4 +33,23 @@ class PreventiveMaintenanceDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(PreventiveMaintenanceDetailView, self).get_context_data(**kwargs)
+        context["location_form"] = LocationForm(instance=self.get_object().asset.location)
+        context["asset_update"] = PreventiveMaintenanceAssetDetailsForm(initial=self.pm_asset_details())
         return context
+
+    def pm_asset_details(self):
+        try:
+            data = {
+                "type": self.get_object().asset.asset_type,
+                "weight": self.get_object().asset.attribute_set.weight,
+                "brand": self.get_object().asset.attribute_set.brand
+            }
+
+        except Asset.DoesNotExist: 
+            data = {
+                "type": None,
+                "weight": None,
+                "brand": None
+            }
+
+        return data            
