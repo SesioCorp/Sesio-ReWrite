124d9f1af880e573043c95bf4f75db34df95855b
alex <alex@penguin>
2022-11-03 08:05:26 -0500

    Created Common Module and Base Model
    Why: For our other models to inheret basic attributes such as
    created_at, updated_at, etc.
    
    How: models.py
    
    Tags:

---
+++ common/models.py
@@ -0,0 +1,66 @@
+from tkinter import CASCADE
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext_lazy as __
+
+
+class BaseQueryset(models.Queryset):
+    def delete(self):
+        for object in self.all():
+            object.delete()
+
+
+class BaseManager(models.Manager):
+    def get_queryset(self):
+        return BaseQueryset(self.model, using=self._db).filter(deleted=False)
+
+    def all_with_deleted(self):
+        return super(BaseManager, self).get_queryset()
+
+    def get_one_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+
+        except ObjectDoesNotExist:
+            return None
+    
+
+class Auditable(models.Model):
+    
+    class Meta:
+        abstract = True
+        default_permissions = ("add", "change", "delete", "view", "view_all")
+
+    objects = BaseManager()
+    deleted = models.BooleanField(null=False, default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    deleted_on = models.DateTimeField(default=None, null=True, blank=True)
+    modified_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
+    modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
+    deleted_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def delete(self, **kwargs):
+        self.deleted = True
+        self.deleted_on = timezone.now()
+        self.save()
+
+    def permanent_delete(self, **kwargs):
+        super().delete(**kwargs)
+
+
+class BaseModel(Auditable):
+
+    class Meta(Auditable.Meta):
+        abstract = True
+        
+
+
+
+
+
+
+    
+
