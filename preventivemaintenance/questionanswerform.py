8e1b8460b9515c8386d41fc15cb256bb41629d3d
alex <alex@penguin>
2022-11-07 09:05:07 -0600

    Added to Question Answer Form
    Why:
    
    How: preventivequestionanswer.py
    
    Tags:

--- preventivemaintenance/questionanswerform.py
+++ preventivemaintenance/questionanswerform.py
@@ -1,6 +1,7 @@
 from genericpath import exists
 from socket import fromshare
 from tkinter.tix import Form
+from unicodedata import category
 from django import forms
 from django.conf import settings
 from django.urls import reverse
@@ -50,6 +51,8 @@ class PreventiveMaintenanceQuestionAnswerForm(forms.models.ModelForm):
     def __init__(self, *args, **kwargs):
 
         #Initialize the Form
+        
+        self.questions = []
 
         self.asset = kwargs.pop("asset")
         self.slug = kwargs.pop("slug")
@@ -83,71 +86,126 @@ class PreventiveMaintenanceQuestionAnswerForm(forms.models.ModelForm):
         self.__add_extra_fields()
 
 
-    def add_questions(self):
+    def question_step(self):
+        if self.step == len(self.categories):
+            question_step = (
+                self.asset.question_set.question.filter(
+                    category__isnull=True, category_id=self.categories[self.step]
+                    ).prefetch_related("question").order_by("order", "id")
+            )
+        elif self.categories[self.step].is_comments:
+            question_step = self.asset.question_set.filter(
+                category_id = self.categories[self.step],
+                deleted = False
+            ).order_by("order", "id")
 
-        #Add questions for each step in the form
+        else:
+            question_step = self.asset.question_set.filter(
+                category_id = self.categories[self.step],
+                parent__isnull = True
+            ).order_by("order", "id")
 
-        if self.step is not None:
-            if self.step == len(self.categories):
-                question_step = (
-                    self.asset.question_set.question.filter(
-                        category__isnull=True, category_id=self.categories[self.step]
-                        ).prefetch_related("question").order_by("order", "id")
-                )
-            elif self.categories[self.step].is_comments:
-                question_step = self.asset.question_set.filter(
-                    category_id = self.categories[self.step],
-                    deleted = False
-                ).order_by("order", "id")
+        return question_step
+
+    def get_child_questions(self):
+        child_questions = self.questions.get_all_child_question_by_category(include_self=False)
+        index = 0
+
+        for child_question in child_questions:
+            answer = (
+                answer if index == 0 else self._get_preexisting_answer(child_question.parent)
+            )
+
+            if child_question is not None and answer.is_fail:
+                self.questions.append(child_question)
 
             else:
-                question_step = self.asset.question_set.filter(
-                    category_id = self.categories[self.step],
-                    parent__isnull = True
-                ).order_by("order", "id")
+                break
 
-            questions = []
+            index += 1
 
-            for question in question_step:
-                questions.append(question)
+    def get_comment_questions(self):
+        for question in self.questions:
+            if question.category.is_comments is True and question.parent:
+                answer = Answer.objects.filter(
+                    question=question,
+                    preventivemaintenance = self.preventivemaintenance
+                )
+                parent_answer = self._get_preexisting_answer(question.parent)
+                parent_child_question = (
+                    question.parent.parent_question.filter(category=question.parent.category).first()
+                    if question.parent.parent_question.filter(category=question.parent.category).exists()
+                    else None
+                )
+
+                if (
+                    answer.exists() and parent_answer and parent_child_question is not None
+                ):
+                    self.add_question(question)
+
+            else:
+                self.add_question(question)
+
+    def add_question(self, question, data=0):
+        kwargs = {"label": question.question_text, "required": False}
+        initial = self.get_question_initial(question, data)
+
+        if initial:
+            kwargs["initial"] = initial
+
+        choices = (self.get_question_choices(question) if question.answer_type in [SELECT, RADIO, SELECT_MULTIPLE] else "")
+
+        if choices:
+            kwargs["choices"] = choices
+        
+        widget = self.get_question_widget(question)
+
+        if widget:
+            kwargs["widget"] = widget
+
+        field = self.get_question_field(question, **kwargs)
+
+        field.widget.attrs["category_id"] = (question.category.pk if question.category else "")
+        
+        field.widget.attrs["comment_category_id"] = (self.asset.question_set.question.filter(category__is_comments=True).first().category.pk
+        if self.asset.question_set.question.filter(category__is_comments=True).exists() else "")
+
+        field.widget.attrs["fail_condition_answer"] = (
+            question.parent_question.filter(category=question.category).first().parent_answer
+            if question.parent_question.filter(category=question.category).exists() else ""
+        )
+        
+        field.widget.attrs["preventivemaintenance_slug"] = (
+            self.preventivemaintenance.slug if self.preventivemaintenance.slug else ""
+        )
+
+        field.widget.attrs["preventivemaintenance_id"] = (
+            self.preventivemaintenance.pk if self.preventivemaintenance else ""
+        )
+
+        field.widget.attrs["facility_id"] = (
+            self.preventivemaintenance.facility.pk if self.preventivemaintenance.facility else ""
+        )
+
+        field.widget.attrs["asset_name"] = (self.asset.slug if self.asset else "")
+
+        field.widget.attrs["asset_id"] = (
+            self.asset.pk if self.asset else ""
+        )
+
+
+
+    def add_questions(self):
+
+        if self.step is not None:
+            for question in self.question_step():
+                self.questions.append(question)
                 answer = self._get_preexisting_answer(question)
 
                 if answer is not None:
-                    child_questions = question.get_all_child_question_by_category(include_self=False)
-                    index = 0
+                    self.get_child_questions()
+                    self.get_comment_questions()
 
-                    for child_question in child_questions:
-                        answer = (
-                            answer if index == 0 else self._get_preexisting_answer(child_question.parent)
-                        )
-
-                        if child_question is not None and answer.is_fail:
-                            questions.append(child_question)
-
-                        else:
-                            break
-
-                        index += 1
-
-                    for question in questions:
-                        if question.category.is_comments is True and question.parent:
-                            answer = Answer.objects.filter(
-                                question=question,
-                                preventivemaintenance = self.preventivemaintenance
-                            )
-                            parent_answer = self._get_preexisting_answer(question.parent)
-                            parent_child_question = (
-                                question.parent.parent_question.filter(category=question.parent.category).first()
-                                if question.parent.parent_question.filter(category=question.parent.category).exists()
-                                else None
-                            )
-
-                            if (
-                                answer.exists() and parent_answer and parent_child_question is not None
-                            ):
-                                self.add_question(question)
-
-                        else:
-                            self.add_question(question)
+    
         
                         
