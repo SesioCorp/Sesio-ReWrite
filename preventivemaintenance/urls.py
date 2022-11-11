626d28e745f15905dd6d050ca3d91af4e9873b9a
philipsesio <pdp_iii@hotmail.com>
2022-11-11 09:45:04 -0600

    PM Template
    Why:To show the question and answer.
    
    How:By using template.
    
    Tags:template

--- preventivemaintenance/urls.py
+++ preventivemaintenance/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PreventiveMaintenanceListView, PreventiveMaintenanceDetailView
+from .views import PreventiveMaintenanceListView, PreventiveMaintenanceDetailView, PreventiveMaintenanceQuestionAnswerView
 from .forms import PreventiveMaintenanceForm
 
 Forms = [
@@ -8,5 +8,9 @@ Forms = [
 
 urlpatterns = [
     path('list/', PreventiveMaintenanceListView.as_view(), name='preventive_maintenance_list'),
-    path('details/<int:pk>/', PreventiveMaintenanceDetailView.as_view(), name='preventive_maintenance_detail')
+    path('details/<int:pk>/', PreventiveMaintenanceDetailView.as_view(), name='preventive_maintenance_detail'),
+    path("question_answer/create/<str:slug>/<int:pk>/<int:step>/", PreventiveMaintenanceQuestionAnswerView.as_view(),
+    name="preventive-maintenance-question-answer-form-step"),
+    path("question_answer/create/<str:slug>/<int:pk>/", PreventiveMaintenanceQuestionAnswerView.as_view(),
+    name="preventive-maintenance-question-answer-form")
 ]
